ae8c40292b30b5b7eb78c3fb73f1b86a4d77e68f
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-23 13:47:34 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+use SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT 
+	COUNT(cancao_id) AS `cancoes`,
+	(SELECT COUNT(artista_id) FROM artista) AS `artistas`,
+	(SELECT COUNT(album_id) FROM album) AS `albuns`
+FROM cancao;
