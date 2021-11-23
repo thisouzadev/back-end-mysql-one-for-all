bbd949244396ecacc9b5e5da14cee1ee95f95a06
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-23 09:34:15 -0300

    correçao de 2 tabelas erro de digitaçao

--- desafio1.sql
+++ desafio1.sql
@@ -81,49 +81,50 @@ VALUES
 
 INSERT INTO cancao(cancao_nome, album_id)
 VALUES
-('Soul For Us', 1),
-('Reflections Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So Do I', 3),
-("Sweetie, Let's Go Wild", 3),
-('She Knows', 3),
-('Fantasy For Me', 4),
-('Celebration Of More', 4),
-('Rock His Everything', 4),
-('Home Forever', 4),
-('Diamond Power', 4),
-("Honey, Let's Be Silly", 4),
-('Thang Of Thunder', 5),
-('Words Of Her Life', 5),
-('Without My Streets', 5);
+	('Soul For Us', 1),
+	('Reflections Of Magic', 1),
+	('Dance With Her Own', 1),
+	('Troubles Of My Inner Fire', 2),
+	('Time Fireworks', 2),
+	('Magic Circus', 3),
+	('Honey, So Do I', 3),
+	("Sweetie, Let's Go Wild", 3),
+	('She Knows', 3),
+	('Fantasy For Me', 4),
+	('Celebration Of More', 4),
+	('Rock His Everything', 4),
+	('Home Forever', 4),
+	('Diamond Power', 4),
+	("Honey, Let's Be Silly", 4),
+	('Thang Of Thunder', 5),
+	('Words Of Her Life', 5),
+	('Without My Streets', 5);
 
-INSERT INTO Reproducoes_por_usuario (usuario_id, cancao_id)
+INSERT INTO reproducoes_por_usuario (usuario_id, cancao_id)
 VALUES
-(1, 1),
-(1, 6),
-(1, 14),
-(1, 16),
-(2, 13),
-(2, 17),
-(2, 2),
-(2, 15),
-(3, 4),
-(3, 16),
-(3, 6),
-(4, 3),
-(4, 18),
-(4, 11);
+	(1, 1),
+	(1, 6),
+	(1, 14),
+	(1, 16),
+	(2, 13),
+	(2, 17),
+	(2, 2),
+	(2, 15),
+	(3, 4),
+	(3, 16),
+	(3, 6),
+	(4, 3),
+	(4, 18),
+	(4, 11);
 
-INSERT INTO Historico_de_seguidores (usuario_id, artista_id)
+INSERT INTO historico_de_seguidores (usuario_id, artista_id)
 VALUES
-(1, 1),
-(1, 4),
-(1, 3),
-(2, 1),
-(2, 3),
-(3, 2),
-(3, 1),
-(4, 4);
+	(1, 1),
+	(1, 4),
+	(1, 3),
+	(2, 1),
+	(2, 3),
+	(3, 2),
+	(3, 1),
+	(4, 4);
+
