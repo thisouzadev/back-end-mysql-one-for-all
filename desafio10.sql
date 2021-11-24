8321edb5447e0b8bd7083eeb68400102941ab814
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-24 07:55:49 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE qtdcancao INT;
+    SELECT COUNT(rpu.usuario_id)
+    FROM reproducoes_por_usuario AS rpu
+    INNER JOIN usuario AS u
+    ON rpu.usuario_id = u.usuario_id
+    WHERE u.usuario_id = id
+    INTO qtdcancao;
+    RETURN qtdcancao;
+END $$
+DELIMITER ;
